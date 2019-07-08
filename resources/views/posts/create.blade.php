088c226ad5007f5a0a7b34457f5ef46cf663af9e
Ryan McCullagh <ryan@amezmo.com>
2019-07-08 15:26:42 -0500

    update site links

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -98,13 +98,8 @@
                 <div class="links">
                     <a href="/queues">Queue tutorial</a>
                     <a href="/hello">Hello</a>
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/posts">Database Tutorial</a>
+                    <a href="https://docs.amezmo.com">Amezmo Docs</a>
                 </div>
             </div>
         </div>
