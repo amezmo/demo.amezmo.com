55e30dcbb3a7ea98df06b5f768a3469ebd4ded1e
Ryan McCullagh <ryan@amezmo.com>
2019-12-01 18:07:07 -0600

    horizon

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,7 +4,10 @@
     <a href="/hello">Hello</a>
     <a href="https://github.com/amezmo/demo.amezmo.com">Source code</a>
     <a href="/posts/new">Database Tutorial</a>
-    <a href="https://docs.amezmo.com">Amezmo Docs</a>
+    <a href="https://docs.amezmo.com">Docs</a>
+    <a href="/register">Register</a>
+    <a href="/register">Login</a>
+    <a href="/register">Horizon</a>
 </div>
 <div>
     <div>Storage path: {{ storage_path() }}</div>
