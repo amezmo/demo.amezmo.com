16e8923065969caed009f962eafb3d23d601dbfc
Ryan McCullagh <ryan@amezmo.com>
2020-01-18 12:45:38 -0600

    fix

--- config/database.php
+++ config/database.php
@@ -62,6 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA      => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_SSL_CERT    => env('MYSQL_ATTR_SSL_CERT'),
                 PDO::MYSQL_ATTR_SSL_KEY     => env('MYSQL_ATTR_SSL_KEY'),
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false
             ]
         ],
 
