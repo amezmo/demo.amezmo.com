015912774b3339221396591a97d52803e9ed9076
Ryan McCullagh <ryan@amezmo.com>
2020-04-23 16:51:30 -0500

    Allow empty

--- resources/views/upload-form.blade.php
+++ resources/views/upload-form.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-
+<h2>Choose your file</h2>
 <form action="/upload" enctype="multipart/form-data" method="post">
     @csrf
     <input type="file" name="user_avatar"/>
