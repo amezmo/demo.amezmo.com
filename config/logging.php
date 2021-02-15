cd7da1ebee64c33253bf8904cec3cb41a3ba9686
Ryan McCullagh <ryan@amezmo.com>
2021-02-15 12:36:24 -0600

    fix log

--- config/logging.php
+++ config/logging.php
@@ -49,7 +49,7 @@ return [
         'daily' => [
             'driver' => 'daily',
             //'permission' => '0664',
-            'path' => env('LOG_PATH', '/webroot/logs/laravel.log'),
+            'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
             'days' => 14,
         ],
