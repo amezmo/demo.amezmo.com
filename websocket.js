6a0c0351d7c75386e6ddb0a1f58f663e51f91f49
Ryan McCullagh <ryan@amezmo.com>
2022-04-15 21:42:47 -0500

    fix

--- websocket.js
+++ websocket.js
@@ -1,34 +1,10 @@
-#!/usr/bin/env nodejs
-
-
-const WebSocketServer = require('socket.io');
-const dotenv = require('dotenv');
-dotenv.config();
-
-// the hook needs to copy the configuration
-const io = new WebSocketServer({
-    origins: ['*']
-});
-
-io.listen(3000);
-
-io.httpServer.on('listening', function() {
-    console.log('Amezmo WebSocket server is listening on %o', io.httpServer.address());
-});
-
-io.sockets.on('connection', function (socket) {
-    console.log('new connection %s', socket.id);
-
-    socket.on('disconnect', function(reason) {
-        console.log('%s: event disconnect: %s', socket.id, reason);
+console.log("Server started");
+var Msg = '';
+var WebSocketServer = require('ws').Server
+    , wss = new WebSocketServer({port: 3000});
+    wss.on('connection', function(ws) {
+        ws.on('message', function(message) {
+        console.log('Received from client: %s', message);
+        ws.send('Server received from client: ' + message);
     });
-    socket.on('error', (error) => {
-        console.log('%s: error with connection %s', socket.id, error);
-        socket.disconnect();
-    });
-
-    socket.on('ping', function() {
-        socket.emit('ping', 'Hello World');
-        console.log('%s: client requested getevents', socket.id);
-    });
-});
+ });
