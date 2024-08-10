6f1b46da382c6166d348df5d6a9d06634349aaca
Ryan McCullagh <ryan@amezmo.com>
2024-08-10 00:30:36 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -77,7 +77,7 @@
                     Sample Laravel Application
                 </div>
                 <p>
-                    This is an <a href="https://www.amezmo.co">Amezmo</a> demo PHP application using Laravel.
+                    This is an <a href="https://www.amezmo.com/?redirs=gh_org_az_r_demo">Amezmo</a> demo PHP application using Laravel.
                     Check out the source code on <a href="https://github.com/amezmo/demo.amezmo.com">GitHub</a>
                 </p>
                 @section('content')
