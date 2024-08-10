7bd4c5c3e64403454f9801f618c72bcec0960880
Ryan McCullagh <ryan@amezmo.com>
2024-08-10 00:31:30 -0500

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -4,7 +4,7 @@
     <a href="/hello">Hello</a>
     <a href="https://github.com/amezmo/demo.amezmo.com">Source code</a>
     <a href="/posts/new">Database Tutorial</a>
-    <a href="https://docs.amezmo.com">Docs</a>
+    <a href="https://www.amezmo.com/docs?redirs=gh_org_az_r_demo_ftr">Docs</a>
     <a href="/chat">Websocket Chat</a>
 </div>
 <div>
